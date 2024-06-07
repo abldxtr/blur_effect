cbb4a4bf75cf38d6df21805fb303f8826322867b
abldxtr <110703641+abldxtr@users.noreply.github.com>
2024-06-07 16:29:38 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Mousemove from "@/components/effect";
+
+export default function Home() {
+  return (
+    <main className=" flex items-center justify-center w-screen h-screen bg-black overflow-hidden ">
+      <Mousemove />
+    </main>
+  );
+}
